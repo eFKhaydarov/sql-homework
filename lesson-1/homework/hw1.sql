3730c636b573fafb953086ed03e2537f4e974536
eFKhaydarov <muhammadali_06x@icloud.com>
2025-06-22 11:37:27 +0500

    hw1.sql

--- lesson-1/homework/hw1.sql
+++ lesson-1/homework/hw1.sql
@@ -1,1 +1,9 @@
-ok
+create database schoolDB
+
+use schoolDB
+create table students (ID int, NAME varchar(50), age int)
+
+select * from students
+insert into students values (1, 'Ali', 19)
+insert into students values (2, 'Asad', 20)
+insert into students values (3, 'Sardor', 21)

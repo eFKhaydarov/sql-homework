a7785e9cc2d00cf35cba137912c851f191048448
eFKhaydarov <muhammadali_06x@icloud.com>
2025-06-23 17:46:14 +0500

    hw1.sql

--- lesson-1/homework/hw1.sql
+++ lesson-1/homework/hw1.sql
@@ -1,9 +1,40 @@
+1. Vazifa
+• Ma’lumot (data): Har qanday raqam, matn yoki boshqa shakldagi axborot.
+• Ma’lumotlar bazasi (database): Ma’lumotlarni saqlash va boshqarish uchun tashkil etilgan tizim.
+• Relatsion ma’lumotlar bazasi (relational database): Jadvallar orqali bog‘langan ma’lumotlar bazasi.
+• Jadval (table): Ma’lumotlar satr (row) va ustunlar (column) shaklida saqlanadigan struktura.
+
+2. Vazifa
+1) Ma’lumotlarni xavfsiz saqlash va boshqarish.
+2) Katta hajmdagi ma’lumotlar bilan ishlash imkoniyati.
+3) Yaxshi ishlov beruvchi so‘rov tizimi (query engine).
+4) Zaxira nusxalash (backup) va tiklash (restore) funksiyalari.
+5) Turli dasturlar bilan integratsiya qilish imkoniyati (Excel, Power BI, .NET va h.k.).
+
+3. Vazifa
+1) Windows Authentication – Windows tizimi orqali kirish.
+2) SQL Server Authentication – Foydalanuvchi nomi va parol orqali kirish.
+
+4. Vazifa
 create database schoolDB
 
+5. Vazifa
 use schoolDB
 create table students (ID int, NAME varchar(50), age int)
 
-select * from students
+6. Vazifa
+• SQL Server – Ma’lumotlar bazasini boshqaruvchi server dasturi.
+• SSMS (SQL Server Management Studio) – SQL Server bilan ishlash uchun grafik interfeysga ega dastur.
+• SQL (Structured Query Language) – Ma’lumotlar bilan ishlash uchun dasturlash tili.
+
+7.Vazifa
+DQL->Data Query Language
+DML->Data Manipulation Language
+DDL->Data Definition Language
+DCL->Data Control Language
+TCL->Transaction Control Language
+
+8.Vazifa
 insert into students values (1, 'Ali', 19)
 insert into students values (2, 'Asad', 20)
 insert into students values (3, 'Sardor', 21)
